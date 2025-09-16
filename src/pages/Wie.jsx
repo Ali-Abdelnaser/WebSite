f9cee6058f737adfc5de3d8365ccfcadb3b36c7b
Ali Abdelnaser <alinaserhema60@gmail.com>
2025-09-16 10:23:49 +0300

    Delete src directory

--- src/pages/Wie.jsx
+++
@@ -1,8 +0,0 @@
-export default function Events() {
-  return (
-    <div style={{ padding: "80px", textAlign: "center" }}>
-      <h1>Events Page</h1>
-      <p>Our events will be displayed here soon 🚀</p>
-    </div>
-  );
-}
